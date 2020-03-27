7f3f8a3b31a1dbc8d7364a08ba6497377a819d1c
yeojongki <kk597@sina.com>
2020-03-28 01:53:49 +0800

    path alias & deps @types/raven

--- src/shared/getTargetRoom.ts
+++
@@ -1,3 +0,0 @@
-// export async function getTargetRoom() {
-//   return room && (await room.topic()).includes(Config.getInstance().ROOM_NAME)
-// }
--- src/shared/getWhiteListMap.ts
+++ src/shared/getWhiteListMap.ts
@@ -1,4 +1,4 @@
-import Config from '@/config'
+import Config from '../config'
 const config = Config.getInstance()
 
 export default function getWhiteListMap() {
